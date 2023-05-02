c3d753a4bd9f36d8bde69e8150946282e1563bc5
1331felipe <felipehuh2023@gmail.com>
2023-05-02 20:23:11 -0300

    array-exercices

---
+++ index.js
@@ -0,0 +1,42 @@
+/*)1) a) a. undefined
+      b) b. null
+      c) c. 11
+      d) d.  3
+      e) array = [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+      f) 9*/
+
+/* 2) "SUBI NUM ÔNIBUS EM MIRROCOS" 27*/
+
+//1)  // const userEmail = prompt("Type your e-mail:")
+// const userName = prompt("What's your name?")
+// console.log("The e-mail", userEmail, "was sucessfuly created, welcome", userName, "!")
+//2)
+// let favouriteFoods = [
+//   "TTONGSUL",
+//   "BABY MOUSE WINE",
+//   "MONKEY BRAIN",
+//   "VIRGIN BOY EGGS",
+//   "SHEEP EYEBALL JUICE",
+// ];
+
+// console.log("Those are your favourite foods:\n", favouriteFoods[0]);
+// console.log("\n", favouriteFoods[1]);
+// console.log("\n", favouriteFoods[2]);
+// console.log("\n", favouriteFoods[3]);
+// console.log("\n", favouriteFoods[4]);
+
+// let userFavouriteFood = prompt("Whats is your favourite food?");
+
+// favouriteFoods[1] = userFavouriteFood;
+
+// console.log (favouriteFoods);
+
+// let taskList = []
+// taskList[0] = prompt("Tell me three tasks you need to accomplish.");
+// taskList[1] = prompt("Tell me two more tasks you need to accomplish.");
+// taskList[2] = prompt("Tell me one more task you need to accomplish.");
+
+// console.log(taskList)
+
+// taskList.splice(Number = prompt("Tell me a task that you already accomplished, from 0 to 2"),1);
+// console.log(taskList);
